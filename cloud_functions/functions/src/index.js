d9d3390d36440d5ff81f87b90aa5133ec3105984
João Nascimento <joaombn97@gmail.com>
2022-04-15 11:19:13 -0300

    Auth through JWT

--- cloud_functions/functions/src/index.js
+++ cloud_functions/functions/src/index.js
@@ -1,10 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { updateSheet } = require('./updateSheet')
+const { updateSheet } = require('./auth');
 
 admin.initializeApp();
 
-// Start writing Firebase Functions
 exports.addToSheet = functions.https.onCall((data, context) => {
     return updateSheet(data.text);
 });
